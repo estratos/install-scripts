cf297c7f370a2692a56664bae5cee9036041bd2f
Estratos Consulting <julio@estratos.net>
2024-09-12 21:11:36 -0600

    Update openvpn.sh

--- setup/install/openvpn/ubuntu/openvpn.sh
+++ setup/install/openvpn/ubuntu/openvpn.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+
+exiterr()  { echo "Error: $1" >&2; exit 1; }
+exiterr2() { exiterr "'apt-get install' fallo."; }
+exiterr3() { exiterr "'yum install' fallo."; }
+exiterr4() { exiterr "'zypper install' fallo."; }
+
+check_os() {
+	if grep -qs "ubuntu" /etc/os-release; then
+		os="ubuntu"
+		os_version=$(grep 'VERSION_ID' /etc/os-release | cut -d '"' -f 2 | tr -d '.')
+		group_name="nogroup"
+  else
+		exiterr "This installer seems to be running on an unsupported distribution.
+Supported distros are Ubuntu"
+	fi
+}
+check_os_ver() {
+	if [[ "$os" == "ubuntu" && "$os_version" -lt 2004 ]]; then
+		exiterr "Ubuntu 20.04 or higher is required to use this installer.
+This version of Ubuntu is too old and unsupported."
+	fi
+ }
+ 
+check_os
+check_os_ver
+
 apt -y update
 apt -y upgrade
 apt -y install ca-certificates wget net-tools gnupg
